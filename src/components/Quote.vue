5a14035b8aa13001d49f204fbc36a80afe9f62ee
Joseph Isbell <shadowwolf347@comcast.net>
2018-10-28 16:36:11 -0600

    Used slot tag to act as placeholder for the comtent passed from parent.

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <p>A wonderful Quote!</p>
+        <slot></slot>
     </div>
 </template>
 
@@ -17,6 +17,10 @@ export default {
         padding: 30px;
         margin: 30px auto;
         text-align: center;
+    }
+
+    h2 {
+        color: red;
     }
 </style>
 
