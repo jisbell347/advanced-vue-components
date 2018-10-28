715447585dec5a2610ed91cfd4a2640fa5d99efa
Joseph Isbell <shadowwolf347@comcast.net>
2018-10-28 16:41:24 -0600

    Created name attributes for each slot to break up the title from the content.

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <slot></slot>
+        <div class="title">
+            <slot name="title"></slot>
+        </div>
+        <hr>
+        <div>
+            <slot name="content"></slot>
+        </div> 
     </div>
 </template>
 
@@ -21,6 +27,9 @@ export default {
 
     h2 {
         color: red;
+    }
+    .title {
+        font-style: italic;
     }
 </style>
 
